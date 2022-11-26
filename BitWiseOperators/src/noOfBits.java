8c920f17f0206409db75ecc4b1a099bca4fee59e
user <user@DESKTOP-UDK2C11>
2022-11-26 23:07:46 +0530

    NoOfBits

---
+++ BitWiseOperators/src/noOfBits.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class noOfBits {
+
+	public static int bits(int n)
+	{
+		int count=0;
+		while(n>0)
+		{
+			n=n&(n-1);
+			count++;
+			
+		}
+		
+		return count;
+		
+	}
+	
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+System.out.println(bits(n));
+	}
+
+}
